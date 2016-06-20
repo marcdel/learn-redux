4b55bf9f37a591878bba779d68dfc7cdbf32a0f3
Marc Delagrammatikas <mdelagrammatikas@truecar.com>
2016-06-19 20:58:17 -0700

    Chapter 5: Create Redux store.

---
+++ client/store.js
@@ -0,0 +1,18 @@
+import { createStore, compose } from 'redux';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { browserHistory } from 'react-router';
+
+import routeReducer from './reducers/index';
+
+import comments from  './data/comments';
+import posts from  './data/posts';
+
+const defaultState = {
+  posts,
+  comments
+};
+
+const store = createStore(routeReducer, defaultState);
+
+export const history = syncHistoryWithStore(browserHistory, store);
+export default store;
