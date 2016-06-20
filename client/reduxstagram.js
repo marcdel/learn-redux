af04f4ac0f4758634b3cf4def5cb9ca3614dea7c
Marc Delagrammatikas <mdelagrammatikas@truecar.com>
2016-06-19 22:40:49 -0700

    Chapter 10: Make dispatch and state accessible to Main component.

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -6,14 +6,14 @@ import { Provider } from 'react-redux';
 import css from './styles/style.styl';
 import store, { history } from './store';
 
-import Main from './components/Main';
+import App from './components/App';
 import Single from './components/Single';
 import PhotoGrid from './components/PhotoGrid';
 
 const router = (
   <Provider store={store}>
     <Router history={history}>
-      <Route path="/" component={Main}>
+      <Route path="/" component={App}>
         <IndexRoute component={PhotoGrid}></IndexRoute>
         <Route path="/view/:postId" component={Single}></Route>
       </Route>
