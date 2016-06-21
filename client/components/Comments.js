e7379756c18981a84170e3f892502d84af457da1
Marc Delagrammatikas <mdelagrammatikas@truecar.com>
2016-06-20 21:20:33 -0700

    Chapter 13: Add Photo to Single and create Comments component and add to Single.

---
+++ client/components/Comments.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Comments extends React.Component {
+  render(){
+    return (
+      <div className="comment">
+      I'm the comments.
+      </div>
+    );
+  }
+}
