fc62b8aba19960830f7a5429cecba65c04ceb495
Marc Delagrammatikas <mdelagrammatikas@truecar.com>
2016-06-20 17:44:16 -0700

    Chapter 11: Add Photo and PhotoGrid layout elements.

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import Photo from './Photo';
+
 export default class PhotoGrid extends React.Component {
   render(){
     return (
       <div className="photo-grid">
-        Photo grid.
+        {this.props.posts.map((post, index) => <Photo key={index} index={index} post={post} {...this.props} />)}
       </div>
     );
   }
